068e3186190c5aa4fd254f3f559ed3f9ca3de866
Norman Kita <kitanorman1@gmail.com>
2025-04-22 09:04:01 +0300

    content

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,7 @@
 @section('content')
     <div class="card card-default">
         <div class="px-6 py-4">
-            <p>Mono provides a variety of <span class="text-secondary text-capitalize"> Bootstrap Basic
-                    inputs </span> components with a
-                little customization that suits its design standards. For more information, please see
-                the official <a class="font-weight-bold"
-                    href="https://getbootstrap.com/docs/4.3/components/forms/" target="_blank">
-                    Bootstrap documentation.</a></p>
+           <p>some contents are to be here</p>
             <x-system.input name="gender" placeholder="Enter your name" />
         </div>
     </div>
