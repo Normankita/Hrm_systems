fb5cb8028be825e93f69a97727fd69a20c9e914e
Norman Kita <kitanorman1@gmail.com>
2025-04-22 09:00:58 +0300

    layout

--- resources/views/_partials/_footer.blade.php
+++ resources/views/_partials/_footer.blade.php
@@ -1,8 +1,7 @@
 <footer class="footer mt-auto">
     <div class="copyright bg-white">
         <p>
-            &copy; <span id="copy-year"></span> Copyright Mono Dashboard Bootstrap Template by <a
-                class="text-primary" href="http://www.iamabdus.com/" target="_blank">Abdus</a>.
+            &copy; <span id="copy-year"></span> Todaysky 
         </p>
     </div>
     <script>
