92a00d5ca5023a8dfdbc000a5351672a8586482a
Norman Kita <kitanorman1@gmail.com>
2025-06-24 23:56:37 -0700

    Giving changes to Feyswal

--- resources/views/employee/manage/allowance_groups/members.blade.php
+++ resources/views/employee/manage/allowance_groups/members.blade.php
@@ -1,149 +1,149 @@
 @php
     use App\Http\Resources\UserResource;
-
 @endphp
+
 @extends('layouts.system')
+
 @section('content')
-    <div class="row justify-content-center" id="emps">
-        <div class="row justify-content-center" v-if="empSubmit">
-            <div class="spinner-border text-primary" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>
-        <div v-if="!empSubmit" class="col-md-10 mb-5">
-            <form action="#">
-                <div class="row justify-content-start align-items-end">
-                    <div class="col-md-5">
-                        <label for="name">Choose Employee</label>
-                        <select v-model="chosen" class="form-control">
-                            <option value="">Choose Employee</option>
-                            <option v-bind:value="employee" v-for="(employee, index) in employees">
-                                @{{ employee.full_name }}
-                            </option>
-                        </select>
-                    </div>
-                    <div class="col-md-3">
-                        <label for="amount">Amount</label>
-                        <input type="text" v-model="amount" class="form-control" placeholder="amount">
-                    </div>
-                    <div class="col-md-2">
-                        <button class="btn btn-sm btn-primary" type="button" v-on:click="addEmployee">add</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-        <div v-show="selectedEmployees.length > 0" class="col-md-10">
-            <div class="mb-3">
-                <h4>SELECTED MEMBERS</h4>
-            </div>
-            <table class="table table-sm table-hover">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(employee, index) in selectedEmployees">
-                        <td>
-                            @{{ employee.full_name }}
-                        </td>
-                        <td>
-                            @{{ employee.amount }}
-                        </td>
-                        <td>
-                            <button class="btn btn-sm btn-danger" type="button"
-                                v-on:click="removeEmployeeFromSelected(index)">
-                                remove</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="col-md-10 mt-2">
-            <button class="btn btn-sm btn-primary" type="button" v-on:click="submitChosenEmployee">submit</button>
+<div class="row justify-content-center" id="emps">
+    <div class="row justify-content-center" v-if="empSubmit">
+        <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
         </div>
     </div>
+
+    <div v-if="!empSubmit" class="col-md-12 mb-4">
+        <!-- DEFAULT CONTROLS -->
+        <div class="row mb-3">
+            <div class="col-md-3">
+                <label>Default Amount</label>
+                <input type="number" class="form-control" v-model="defaultAmount" v-on:input="applyDefaultAmount">
+            </div>
+            <div class="col-md-3">
+                <label>Default Frequency</label>
+                <select class="form-control" v-model="defaultFrequency" v-on:change="applyDefaultFrequency">
+                    <option value="">-- Select Frequency --</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select>
+            </div>
+            <div class="col-md-2 d-flex align-items-end">
+                <button class="btn btn-sm btn-secondary" v-on:click="selectAll">Select All</button>
+            </div>
+        </div>
+
+        <!-- EMPLOYEE TABLE -->
+        <table class="table table-bordered table-sm">
+            <thead>
+                <tr>
+                    <th><input type="checkbox" v-on:change="toggleAll($event)"></th>
+                    <th>Name</th>
+                    <th>Amount</th>
+                    <th>Frequency</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(employee, index) in employees" :key="employee.id">
+                    <td>
+                        <input type="checkbox" v-model="employee.selected">
+                    </td>
+                    <td>@{{ employee.full_name }}</td>
+                    <td>
+                        <input type="number" class="form-control" v-model="employee.amount">
+                    </td>
+                    <td>
+                        <select class="form-control" v-model="employee.frequency">
+                            <option value="">--</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                        </select>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div class="text-end mt-2">
+            <button class="btn btn-sm btn-primary" type="button" v-on:click="submitSelectedEmployees">Submit</button>
+        </div>
+    </div>
+</div>
 @endsection
+
 @php
-    // extract user by resource
     $user = new UserResource(auth()->user());
 @endphp
 
 @section('scripts')
-    <script>
-        // Initialize the handler and store the instance
-        const handler1 = new TableSelectionHandler('.dt-table', '.all-checker');
+<script>
+    const employees = {!! json_encode($employees) !!};
+    const allowanceGroup = {!! json_encode($group) !!};
+    const user = {!! json_encode($user) !!};
 
-        const employees = {!! json_encode($employees) !!};
-        const allowanceGroup = {!! json_encode($group) !!};
-        const user = {!! json_encode($user) !!};
-
-        const app = Vue.createApp({
-            data() {
-                return {
-                    employees: employees,
-                    selectedEmployees: [],
-                    chosen: '',
-                    empSubmit: false,
-                    groupMambers: [],
-                    group: allowanceGroup,
-                    error: null,
-                    user: user,
-                    amount: 0,
-                    removedIndexes: [],
-                };
+    const app = Vue.createApp({
+        data() {
+            return {
+                employees: employees.map(e => ({ ...e, selected: false, amount: null, frequency: null })),
+                defaultAmount: '',
+                defaultFrequency: '',
+                empSubmit: false,
+                group: allowanceGroup,
+                user: user,
+                error: null,
+            };
+        },
+        methods: {
+            selectAll() {
+                this.employees.forEach(e => e.selected = true);
             },
-            methods: {
-                addEmployee() {
-                    if (this.chosen == '') {
-                        return;
+            toggleAll(event) {
+                const checked = event.target.checked;
+                this.employees.forEach(e => e.selected = checked);
+            },
+            applyDefaultAmount() {
+                this.employees.forEach(e => {
+                    if (e.selected) e.amount = this.defaultAmount;
+                });
+            },
+            applyDefaultFrequency() {
+                this.employees.forEach(e => {
+                    if (e.selected) e.frequency = this.defaultFrequency;
+                });
+            },
+            submitSelectedEmployees() {
+                this.empSubmit = true;
+                const selected = this.employees
+                    .filter(e => e.selected)
+                    .map(e => ({
+                        id: e.id,
+                        full_name: e.full_name,
+                        amount: e.amount,
+                        frequency: e.frequency
+                    }));
+
+                if (selected.length === 0) {
+                    this.empSubmit = false;
+                    alert("No employees selected.");
+                    return;
+                }
+
+                axios.post(`/api/groups/add/employees/to/group/${this.group.id}`, {
+                    user: this.user,
+                    employees: selected
+                })
+                .then(res => {
+                    if (res.data.status === 'success') {
+                        location.reload();
+                    } else {
+                        this.empSubmit = false;
+                        this.error = "Failed to submit.";
                     }
-                    this.chosen.amount = this.amount;
-                    this.selectedEmployees.push(this.chosen);
-                    this.employees.splice(this.employees.indexOf(this.chosen), 1);
-                },
-                removeEmployeeFromSelected(index) {
-                    this.employees.push(this.selectedEmployees[index]);
-                    this.selectedEmployees.splice(index, 1);
-                },
-                submitChosenEmployee() {
-                    this.empSubmit = true;
-                    let embeded = {
-                        user: this.user,
-                        employees: this.selectedEmployees
-                    };
-                    const route = `/api/groups/add/employees/to/group/${this.group.id}`;
-                    axios.post(route, embeded)
-                        .then(response => {
-                            const data = response.data;
-                            if (data.status == 'success') {
-                                location.reload();
-                            }
-                        })
-                        .catch(error => {
-                            this.empSubmit = false;
-                            this.error = "Something went wrong, refresh page and try again";
-                        });
-
-                },
-                deleteEmployeeFromGroup() {
-                    let dt = handler1.getSelected();
-                    const route = `/api/groups/remove/employees/from/group/${this.group.id}`;
-                    axios.post(route, {employees: handler1.getSelected(), user: this.user})
-                        .then(response => {
-                            const data = response.data;
-                            if (data.status == 'success') {
-                                location.reload();
-                            }
-                        })
-                        .catch(error => {
-                            this.empSubmit = false;
-                            this.error = "Something went wrong, refresh page and try again";
-                        });
-                },
-            },
-
-        }).mount('#emps');
-    </script>
+                })
+                .catch(() => {
+                    this.empSubmit = false;
+                    this.error = "Error occurred. Try again.";
+                });
+            }
+        }
+    }).mount('#emps');
+</script>
 @endsection
